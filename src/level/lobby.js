3e7d41c60e47bb041a04e791288f2580168bab36
Cameron Taylor <TheJimmyBlaze@gmail.com>
2024-05-28 13:03:36 +1200

    All the lobby colliders? :)

--- src/level/lobby.js
+++ src/level/lobby.js
@@ -100,13 +100,13 @@ export const useLobby = ({
 
         //Stair desk
         useRectCollider({
-            position: usePosition({x: -74, y: -48}),
-            width: 22, height: 64,
+            position: usePosition({x: -72, y: -48}),
+            width: 18, height: 64,
             drawCamera
         }),
         useRectCollider({
-            position: usePosition({x: -130, y: -40}),
-            width: 94, height: 48,
+            position: usePosition({x: -128, y: -41}),
+            width: 98, height: 46,
             drawCamera
         }),
 
@@ -141,13 +141,45 @@ export const useLobby = ({
             drawCamera
         }),
         useRectCollider({
-            position: usePosition({x: 506, y: -296}),
-            width: 20, height: 92,
+            position: usePosition({x: 504, y: -296}),
+            width: 16, height: 92,
             drawCamera
         }),
         useRectCollider({
             position: usePosition({x: 346, y: -121}),
             width: 341, height: 270,
+            drawCamera
+        }),
+
+        //Top Left Corridor
+        useRectCollider({
+            position: usePosition({x: -216, y: -384}),
+            width: 208, height: 160,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -392, y: -322}),
+            width: 16, height: 136,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -380, y: -392}),
+            width: 28, height: 16,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -328, y: -392}),
+            width: 20, height: 16,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -352, y: -394}),
+            width: 32, height: 16,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -347, y: -121}),
+            width: 342, height: 270,
             drawCamera
         }),
 
@@ -244,6 +276,102 @@ export const useLobby = ({
         useRectCollider({
             position: usePosition({x: 333, y: 16}),
             width: 4, height: 4,
+            drawCamera
+        }),
+
+        //Bottom Left Corridor
+        useRectCollider({
+            position: usePosition({x: -260, y: 8}),
+            width: 60, height: 16,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -336, y: 8}),
+            width: 36, height: 16,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -416, y: 8}),
+            width: 68, height: 16,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -488, y: 8}),
+            width: 20, height: 16,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -504, y: 40}),
+            width: 16, height: 64,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -190, y: 22}),
+            width: 28, height: 20,
+            drawCamera
+        }),
+
+        //Left Stairs
+        useRectCollider({
+            position: usePosition({x: -207, y: 28}),
+            width: 4, height: 4,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -212, y: 26}),
+            width: 4, height: 4,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -217, y: 24}),
+            width: 4, height: 4,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -222, y: 22}),
+            width: 4, height: 4,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -227, y: 20}),
+            width: 4, height: 4,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -232, y: 18}),
+            width: 4, height: 4,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -237, y: 16}),
+            width: 4, height: 4,
+            drawCamera
+        }),
+
+        //Entrance
+        useRectCollider({
+            position: usePosition({x: 0, y: 504}),
+            width: 128, height: 16,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -339, y: 264}),
+            width: 360, height: 400,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: -113, y: 472}),
+            width: 100, height: 80,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: 299, y: 264}),
+            width: 280, height: 400,
+            drawCamera
+        }),
+        useRectCollider({
+            position: usePosition({x: 113, y: 472}),
+            width: 100, height: 80,
             drawCamera
         }),
     ];
